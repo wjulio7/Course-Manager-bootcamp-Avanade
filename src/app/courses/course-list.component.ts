12497914cf7f83832652f294b25c477e90c7b8c5
wjulio <wolgranjulio@gmail.com>
2021-01-30 19:47:27 -0300

    Lidando com varios componentes

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -3,15 +3,28 @@ import {Course} from './course';
 import {CourseService} from './course.service';
 
 @Component({
-    selector: 'app-course-list',
     templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit{
-    courses: Course [] = [];
+    _courses: Course [] = [];
+    _filterBy: string;
+    filteredCourses: Course [] = [];
+
     constructor(private courseService: CourseService) {
     }
     ngOnInit(): void {
-       this.courses = this.courseService.retrieveAll();
+       this._courses = this.courseService.retrieveAll();
+       this.filteredCourses = this._courses;
+
     }
 
+  set filter(value: string) {
+    this._filterBy = value;
+
+    this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+  }
+
+  get filter() {
+    return this._filterBy;
+  }
 }
